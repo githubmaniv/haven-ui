5c2ae77741cd845eca1408663fa851685408b532
752571 <Mani.Narasimhan@fedex.com>
2022-03-30 11:18:28 -0400

    Added filters and changed layout , renamed the file

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({'name':'Haven',
+                    'greeting':'Hello World!'})
+
+@app.route('/zipcode-rating')
+def zipcode_rating():
+    return jsonify([{'id':'01001',
+                    'state':'Alabama',
+                    'county':'Autauga County',
+                    'rate':5.1}])
 
 if __name__ == '__main__':
     app.run()
