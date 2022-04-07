cdf0ad1cbc3e655152d5fa2f0c291c04c31b6413
752571 <Mani.Narasimhan@fedex.com>
2022-04-07 19:44:25 -0400

    fetching rate from counties table in sqldb

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask import jsonify
+import sqlite3
+import json
 app = Flask(__name__)
-
+DB = "./data/home.db"
 @app.route('/')
 def hello_world():
     return jsonify({'name':'Haven',
                     'greeting':'Hello World!'})
 
-@app.route('/zipcode-rating')
-def zipcode_rating():
-    return jsonify([{'id':'01001',
-                    'state':'Alabama',
-                    'county':'Autauga County',
-                    'rate':5.1}])
+@app.route('/county-rating')
+def county_rating():
+    conn = sqlite3.connect( DB )
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
+    db = conn.cursor()
+    rows = db.execute('''
+    SELECT id,county,state,rate from counties
+    ''').fetchall()
+    conn.close()
+    return json.dumps( [dict(ix) for ix in rows] ) #CREATE JSON
+
+
+
 
 if __name__ == '__main__':
+
     app.run()
